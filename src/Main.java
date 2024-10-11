d50138e157881dc082b931c370799226e0531151
s30835 <s30835@pjwstk.edu.pl>
2024-10-11 16:08:34 +0200

    Update Main.java
    Added your current task

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,11 @@ public class Main {
     public static void main(String[] args) {
         Adder adder = new Adder();
         System.out.println(adder.add(1,2));
+        System.out.println(adder.add(3,4));
+
 
         Substractor substractor = new Substractor();
         System.out.println(substractor.substract(1,2));
+        System.out.println(substractor.substract(100,99));
     }
 }
