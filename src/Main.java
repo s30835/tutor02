61e150fd889d0820ea779dcc89562c1de4f53cfa
Kyrylo Alieksieiev <alieksieiev@Sadie.local>
2024-10-11 16:11:44 +0200

    First commit

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Adder adder = new Adder();
         System.out.println(adder.add(1,2));
-        System.out.println(adder.add(3,4));
-
 
         Substractor substractor = new Substractor();
         System.out.println(substractor.substract(1,2));
-        System.out.println(substractor.substract(100,99));
     }
 }
--- src/Substractor.java
+++
@@ -1,5 +0,0 @@
-public class Substractor {
-    public int substract(int a, int b) {
-        return a - b;
-    }
-}
